d9b6bc60345bee801231a24646ad316732913fe0
David De Los Santos <ddelossantosmateo@gmail.com>
2017-12-21 20:42:56 -0400

    trying to display maps

--- Practica4-GPSApp-1066600/www/js/app.js
+++ Practica4-GPSApp-1066600/www/js/app.js
@@ -3,44 +3,54 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('GPSApp', ['ionic','ngCordova'])
+angular.module('Practica4-GPSApp-1066600', ['ionic','ngCordova'])
 
 .controller('GPSController', function($scope, $cordovaGeolocation) {
   var posOptions = {timeout: 10000, enableHighAccuracy: true};
-  $scope.lat
-  $scope.long
-  $cordovaGeolocation
   
+  $scope.long
+  $scope.lat
+  $scope.map = undefined;
+  $scope.latLong = undefined;
+  
+  
+  $cordovaGeolocation
 
   .getCurrentPosition(posOptions)
 
   .then(function (position) {
     $scope.lat  = position.coords.latitude
     $scope.long = position.coords.longitude
-    console.log( $scope.lat + '   ' +  $scope.long)
+    
+    
   }, function(err) {
      console.log(err)
   });
 
-  var watchOptions = {timeout : 3000, enableHighAccuracy: true};
-  var watch = $cordovaGeolocation.watchPosition(watchOptions);
- 
-  watch.then(
-     null,
-   
-     function(err) {
-        console.log(err)
-     },
-   
-     function(position) {
-         $scope.lat  = position.coords.latitude
-         $scope.long = position.coords.longitude
-        console.log( $scope.lat + '' +  $scope.long)
-     }
-  );
+  var mapOptions = {
+    center: new google.maps.LatLng(0, 0),
+    zoom: 12,
+    mapTypeId: google.maps.MapTypeId.ROADMAP};
 
-  watch.clearWatch();
+
+ $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+
+$scope.latLong = new google.maps.LatLng($scope.lat, $scope.long);
+
+$scope.marker = new google.maps.Marker({
+    position: $scope.latLong
+});
+
+$scope.marker.setMap($scope.map);
+$scope.map.setZoom(15);
+$scope.map.setCenter($scope.marker.getPosition());
+
+
 })
+
+
+
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
